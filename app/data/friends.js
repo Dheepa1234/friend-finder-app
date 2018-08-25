e869a6322aa38ae52eac537d886396df897f4583
Dheepa Sivakumaran <dheepasivakumaran@Dheepas-MacBook-Pro.local>
2018-08-25 17:34:42 -0400

    Please add all changes

---
+++ app/data/friends.js
@@ -0,0 +1,165 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'Sergei Polunin',
+		photo: 'assets/images/Sergei-Polunin.jpeg',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Tessa Virtue',
+		photo: 'assets/images/Tessa-Virtue.jpeg',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Scott Moir',
+		photo: 'assets/images/Scott-Moir.jpeg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Sidney Crosby',
+		photo: 'assets/images/Sidney-Crosby.jpeg',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Gareth Southgate',
+		photo: 'Gareth-Southgate.jpeg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'David Beckham',
+		photo: 'http://img.mindbodygreen.com/image/upload/c_limit,w_738,f_auto/ftr/Miss-Piggy-large-tonya-312.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Tom Brady',
+		photo: 'assets/images/Tom-Brady.jpeg',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Roger Federer',
+		photo: 'https://i1.imgiz.com/data/videoshots/6829/6829475-6461.jpg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Ekaterina Gordeeva',
+		photo: 'assets/images/Ekaterina-Gordeeva.jpeg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Yuna Kim',
+		photo: 'assets/images/Yuna-Kim.jpeg',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;

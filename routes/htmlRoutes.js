7828a3a3ef1025dd185db8f98e26f254510fc6ca
Dheepa Sivakumaran <dheepasivakumaran@Dheepas-MacBook-Pro.local>
2018-08-31 01:14:47 -0400

    Update llogic

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,20 +1,27 @@
-//Pull in data
-
-
-// Pull in required dependencies
+//Node Dependencies
 var path = require('path');
 
-// Export HTML routes
-module.exports = function(app) {
-	// console.log('___ENTER htmlRoutes.js___');
+// Includes Two Routes
+function htmlRoutes(app) {
 
-	// Home page
-	app.get('/', function(req, res) {
-		res.sendFile(path.join(__dirname, '../public/home.html'));
-	});
+  // A GET Route to /survey which should display the survey page.
+  app.get('/survey', function (req, res) {
+    res.sendFile(path.join(__dirname + '/../public/survey.html'));
+  });
 
-	// Survey page
-	app.get('/survey', function(req, res) {
-		res.sendFile(path.join(__dirname, '../public/survey.html'));
-	});
-};
+  // A default USE route that leads to home.html which displays the home page.
+  app.use(function (req, res) {
+    res.sendFile(path.join(__dirname + '/../public/home.html'));
+  });
+
+}
+
+// Export for use in main server.js file
+module.exports = htmlRoutes;
+
+	
+
+
+ 
+
+ 
